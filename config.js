a440568a5ae5d09c463a4a37311df338163a6199
ayoser <106051450+ayoser@users.noreply.github.com>
2022-08-12 15:00:26 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,43 +1,43 @@
 //=============『 Utama 』================== //
-global.owner = ['6287892711054'] 
+global.owner = ['94768678590'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Hyzer Official'
-global.numberowner = '6287892711054'
-global.instagram = 'https://instagram.com/hyzer_stfu'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '087892711054'
-global.pulsa = '085212023766'
-global.gopay = '087892711054'
+global.nameowner = 'Ayodya Vichakshana💕'
+global.numberowner = '94768678590'
+global.instagram = 'm.kn'
+global.github = 'm.kn'
+global.dana = '94768678590'
+global.pulsa = '94768678590'
+global.gopay = '94772496127'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'Yuno Gasai Bot'
-global.gc = 'bit.ly/AnyaBot-Group'
-global.web = 'https://github.com/Hyzerr' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
+global.namebot = 'Garfield MD'
+global.gc = 'bit.ly/GarfieldBot-Group'
+global.web = 'm.kn' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.price1 = '1 day = 10.000\n2 week 7.000\n1 month 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/bf57d2f268b210786d3bc.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/e6af50ec99dc02276b71a.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'ꜱʜɪʀᴏ ɪᴋᴀꜱᴀᴋɪ ʙᴏᴛ'
 global.watermark = wm
-global.wm2 = '                     「 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳 あ⁩ 」'
-global.wm3 = '𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.wm2 = '                     「 𝖦𝖺𝖱𝖥𝗂𝖾𝖫𝖣-𝙼𝙳 あ⁩ 」'
+global.wm3 = '𝙶𝚊𝚛𝚏𝚒𝚎𝚕𝚍-𝙼𝙳'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.media = 'https://telegra.ph/file/d62276c3879ab7428dcf7.png'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wait = '_*tunggu sedang di proses...*_'
+global.wait = ' 𝙥𝙡𝙚𝙖𝙨𝙚 𝙬𝙖𝙞𝙩'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
-global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = '2022'
-global.author = '© Hyzer Official'
+global.stiker_wait = '𝚙𝚕𝚎𝚊𝚜𝚎 𝚠𝚊𝚒𝚝'
+global.packname = '𝙶𝚊𝚛𝚏𝚒𝚎𝚕𝚍'
+global.author = '𝙰𝚢𝚘𝚍𝚢𝚊'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
