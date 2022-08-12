5940245b862aac08ff0566d1e1d8bdab8f7edbeb
ayoser <106051450+ayoser@users.noreply.github.com>
2022-08-12 15:06:41 +0530

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -3,18 +3,18 @@ let name = await conn.getName(m.sender)
   let wm = global.wm
 hyzer = {
     key : {
-  remoteJid: 'status@broadcast',
+  remoteJid: 'status@broadcast@ad',
   participant : '0@s.whatsapp.net'
     },
   message: { 
   "extendedTextMessage": {
-  "text": '*Sama-Sama*',
+  "text": '*my friends*',
   "title": wm,
-  'jpegThumbnail': 'https://telegra.ph/file/20509b31b6e05ff999daa.jpg',
+  'jpegThumbnail': 'https://telegra.ph/file/d62276c3879ab7428dcf7.png',
         }
        } 
      }
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/06a27a93b66f63a5a067e.jpg', hyzer, { packname: "sticker by", author: "hyzer" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/bf57d2f268b210786d3bc.jpg', hyzer, { packname: "sticker by", author: "hyzer" })
 }
 
 handler.customPrefix = /^(Terima Kasih|Thanks|tq)$/i
